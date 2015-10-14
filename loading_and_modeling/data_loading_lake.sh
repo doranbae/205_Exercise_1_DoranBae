3be76eeac067658b5cd93bd456d914085f9f35e8
Doran Bae <dorandoran@gmail.com>
2015-10-15 00:24:58 +0900

    update

--- loading_and_modeling/data_loading_lake.sh
+++ loading_and_modeling/data_loading_lake.sh
@@ -7,10 +7,8 @@ tail -n +2 "Readmissions and Deaths - Hospital.csv" > "readmissions.csv"
 tail -n +2 "Measure Dates.csv" > "measuredates.csv"
 tail -n +2 "hvbp_hcahps_08_06_2015.csv" > "surveys_responses.csv"
 
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/effective_care.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/tmp
-effective_care.csv
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/effective_care.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/usr/w205/hospital_compare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/hospitals.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/usr/w205/hospital_compare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/measuredates.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/usr/w205/hospital_compare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/readmissions.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/usr/w205/hospital_compare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/surveys_responses.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/usr/w205/hospital_compare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/effective_care.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/hospitals.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/measuredates.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/readmissions.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/surveys_responses.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
---
+++ loading_and_modeling/hive_bash_ddl.sql
@@ -0,0 +1,1 @@
+ssh -i "thisKeyPair2.pem" root@54.208.163.51
