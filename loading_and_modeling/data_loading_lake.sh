17d631544d014fb808d40650ccd84288f969c4fd
Doran Bae <dorandoran@gmail.com>
2015-10-17 20:56:01 +0900

    new update

--- loading_and_modeling/data_loading_lake.sh
+++ loading_and_modeling/data_loading_lake.sh
@@ -7,8 +7,8 @@ tail -n +2 "Readmissions and Deaths - Hospital.csv" > "readmissions.csv"
 tail -n +2 "Measure Dates.csv" > "measuredates.csv"
 tail -n +2 "hvbp_hcahps_08_06_2015.csv" > "surveys_responses.csv"
 
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/effective_care.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/hospitals.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/measuredates.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/readmissions.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/surveys_responses.csv root@ec2-54-173-134-33.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/effective_care.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/hospitals.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/measuredates.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/readmissions.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
+scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/surveys_responses.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
