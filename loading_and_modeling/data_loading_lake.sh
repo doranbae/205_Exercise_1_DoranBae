93521a934685029bf21135976ed28c18a33fb9f0
Doran Bae <dorandoran@gmail.com>
2015-11-27 14:41:23 +0900

    Revised version

--- loading_and_modeling/data_loading_lake.sh
+++ loading_and_modeling/data_loading_lake.sh
@@ -1,14 +1,28 @@
-cd Exercise
-cd Datacopy
+#This is script for downlading datafiles
+
+#create a new Directory
+mkdir /data/w205/hospitalcare
+
+cd /data/w205/hospitalcare
+
+#download hospital data
+wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+
+#unzip files
+unzip Hospital_Revised_Flatfiles.zip
+
 cd Hospital_Revised_Flatfiles
+#modify file name & remove header
+
 tail -n +2 "Hospital General Information.csv" > "hospitals.csv"
 tail -n +2 "Timely and Effective Care - Hospital.csv" > "effective_care.csv"
 tail -n +2 "Readmissions and Deaths - Hospital.csv" > "readmissions.csv"
 tail -n +2 "Measure Dates.csv" > "measuredates.csv"
 tail -n +2 "hvbp_hcahps_08_06_2015.csv" > "surveys_responses.csv"
 
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/effective_care.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/hospitals.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/measuredates.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/readmissions.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
-scp -i thisKeyPair2.pem /Users/Doran/Exercise_1_data/surveys_responses.csv root@ec2-54-84-142-229.compute-1.amazonaws.com:/data/w205/hospitalcare
+#copy files back to hospitalcare Directory
+cp /data/w205/hospitalcare/Hospital_Revised_Flatfiles/"hospitals.csv" /data/w205/hospitalcare/
+cp /data/w205/hospitalcare/Hospital_Revised_Flatfiles/"effective_care.csv" /data/w205/hospitalcare/
+cp /data/w205/hospitalcare/Hospital_Revised_Flatfiles/"readmissions.csv" /data/w205/hospitalcare/
+cp /data/w205/hospitalcare/Hospital_Revised_Flatfiles/"measuredates.csv" /data/w205/hospitalcare/
+cp /data/w205/hospitalcare/Hospital_Revised_Flatfiles/"surveys_responses.csv" /data/w205/hospitalcare/
